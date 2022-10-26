f8b8c3fcb30c69027247f4ef0e5fc7406bd0ebfe
CristóbalO_Kronos <dondoncronos@gmail.com>
2022-10-26 14:11:25 -0300

    Create Main.py and the basic structure
    Se define el main en 4 partes principales, la documentación, los elementos importados, las funciones a usar y el main

---
+++ Main.py
@@ -0,0 +1,20 @@
+""" Documents
+    Nombre: Cristóbal Osses Obreque
+
+    Enunciado:
+        a
+"""
+
+#IMPORT's
+import numpy as np
+
+
+#FUNCTIONS
+def menu():
+    print("Hola Mundo")
+
+
+#MAIN
+
+
+
